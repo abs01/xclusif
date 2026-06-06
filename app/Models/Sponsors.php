61b0b3ab58594f4116cce7fae6b3fdfffdaecb92
Antoni <Antoni>
2026-06-06 12:55:15 +0200

    comentario

--- app/Models/Sponsors.php
+++ app/Models/Sponsors.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Sponsors extends Model
+//Esta tabla está aislada del resto de la estructura. Principalmente con el objetivo de guardar la publicidad que se muestra en la plataforma
 {
     protected $fillable = [
         'company_name',
